531493bfbba535072165e0e87704d887dec73847
Kanitin Juthasompakorn <kanitin.ju@gmail.com>
2026-01-08 10:55:57 +0700

    feat:add exercise 2 answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react";
 import "./App.css";
-
 function App() {
+  const [inputText, setInputText] = useState(""); 
+  const [submittedText, setSubmittedText] = useState("Hello DT"); 
+
+  const handleAddClick = () => {
+    setSubmittedText(inputText); 
+  }
   return (
     <div className="App">
       <h1>Simple Greeting Message App 2</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
-        <input type="text" />
+        <h2>{submittedText}</h2>
+        <input type="text" value={inputText} onChange={(event) => setInputText(event.target.value)} />
         <div className="buttons">
-          <button>Add</button>
+          <button onClick={handleAddClick}>Add</button>
         </div>
       </div>
     </div>
