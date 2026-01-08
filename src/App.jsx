ac64f6da74a60aab6abbd163b97cf286e4510cdd
Kanitin Juthasompakorn <kanitin.ju@gmail.com>
2026-01-08 10:10:22 +0700

    feat:add exercise1 answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import { useState } from "react";
 import "./App.css";
-
+const [greeting, setGreeting] = useState("");
 function App() {
   return (
     <div className="App">
       <h1>Simple Greeting Message App</h1>
       <div className="greeting-container">
-        <h2>ผลลัพธ์จะมาแทนที่ข้อความนี้</h2>
+        <h2>{greeting}</h2>
         <div className="buttons">
-          <button>Hi</button>
-          <button>สวัสดี</button>
+          <button onClick={() => setGreeting("Hi")}>Hi</button>
+          <button onClick={() => setGreeting("Hello")}>สวัสดี</button>
         </div>
       </div>
     </div>
